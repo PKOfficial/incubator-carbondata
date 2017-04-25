203a8e3f0bf54434dae1d61bfdd6af65515de26b
Sangeeta Gulia <sangeeta.gulia@knoldus.in>
2017-04-25 16:08:26 +0530

    Added CarbonDataFlinkInputFormat to read from carbondata using Flink

---
+++ integration/flink/src/test/java/org/apache/carbondata/flink/CarbonFlinkInputFormatBenchmarkTest.java
@@ -0,0 +1,36 @@
+package org.apache.carbondata.flink;
+
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CarbonFlinkInputFormatBenchmarkTest {
+
+    String getRootPath() throws IOException {
+        return new File(this.getClass().getResource("/").getPath() + "../../../..").getCanonicalPath();
+    }
+
+    @Test
+    public void getDataFromCarbon() throws Exception {
+        long t1 = System.currentTimeMillis();
+
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        String[] columns = {"CUST_ID", "CUST_NAME", "ACTIVE_EMUI_VERSION", "DOB", "DOJ", "BIGINT_COLUMN1", "BIGINT_COLUMN2", "DECIMAL_COLUMN1", "DECIMAL_COLUMN2", "Double_COLUMN1", "Double_COLUMN2", "INTEGER_COLUMN1"};
+        String path = "/integration/flink/src/test/resources/store/default/uniqdata";
+        CarbonDataFlinkInputFormat carbondataFlinkInputFormat = new CarbonDataFlinkInputFormat(getRootPath() + path, columns, false);
+        DataSource<Tuple2<Void, Object[]>> dataSource = env.createInput(carbondataFlinkInputFormat.getInputFormat());
+        int rowCount = dataSource.collect().size();
+        assert (rowCount == 100);
+        long t2 = System.currentTimeMillis();
+        long timeTaken = t2 - t1;
+        System.out.println("Time taken : " + timeTaken);
+        //assert(timeTaken < 1500);
+
+
+    }
+}

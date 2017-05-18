10214500527235d67adffd99b03be64a0a9f3f65
ksimar <simarpreetkaurmonga1993@gmail.com>
2017-05-18 13:56:54 +0530

    done Ready for review

--- streaming/src/main/scala/writing/CarbonStreamingLoadModel.scala
+++ streaming/src/main/scala/writing/CarbonStreamingLoadModel.scala
@@ -1,12 +1,38 @@
 package writing
 
-class CarbonStreamingLoadModel(val databaseName: String, val tableName: String, val carbonDataFileName: String,
-                               val partitionId: String, val segmentId: String,
-                               val storePath: String, val sessionId: String, val factFilePath: String,
-                               val factTimeStamp: String, val badRecordsLoggerEnable: String,
-                               val badRecordsAction: String, val maxColumns: String, val isEmptyDataBadRecord: String
-                              ) extends Serializable{
+import org.apache.carbondata.core.constants.CarbonCommonConstants
+import org.apache.carbondata.core.metadata.CarbonTableIdentifier
+import org.apache.carbondata.core.mutate.SegmentUpdateDetails
+import org.apache.carbondata.core.statusmanager.{LoadMetadataDetails, SegmentUpdateStatusManager}
+import org.apache.carbondata.processing.model.CarbonDataLoadSchema
+import org.apache.carbondata.processing.newflow.constants.DataLoadProcessorConstants
 
+case class CarbonStreamingLoadModel(carbonTableIdentifier: CarbonTableIdentifier,
+                                    partitionId: String, streamingSegmentId: String,
+                                    storePath: String,
+                                    factFilePath: String, factTimeStamp: String,
+                                    badRecordsLoggerEnable: String,
+                                    badRecordsAction: String, maxColumns: String,
+                                    isEmptyDataBadRecord: String,
+                                    carbonDataLoadSchema: CarbonDataLoadSchema,
+                                    aggTables: Array[String],
+                                    aggTableName: String,
+                                    isDirectLoad: Boolean,
+                                    loadMetadataDetails: List[LoadMetadataDetails],
+                                    @transient segmentUpdateDetails: List[SegmentUpdateDetails],
+                                    @transient segmentUpdateStatusManager: SegmentUpdateStatusManager
+                                   ) extends Serializable {
+
+  def this(carbonTableIdentifier: CarbonTableIdentifier, streamingSegmentId: String, maxColumns: String, carbonDataLoadSchema: CarbonDataLoadSchema,
+           aggTables: Array[String], aggTableName: String, isDirectLoad: Boolean, loadMetadataDetails: List[LoadMetadataDetails],
+           segmentUpdateDetails: List[SegmentUpdateDetails], segmentUpdateStatusManager: SegmentUpdateStatusManager) = {
+
+    this(carbonTableIdentifier, "0", streamingSegmentId, CarbonCommonConstants.STORE_LOCATION_DEFAULT_VAL, DataLoadProcessorConstants.FACT_FILE_PATH,
+      DataLoadProcessorConstants.FACT_TIME_STAMP, DataLoadProcessorConstants.BAD_RECORDS_LOGGER_ENABLE, DataLoadProcessorConstants.BAD_RECORDS_LOGGER_ACTION,
+      maxColumns, DataLoadProcessorConstants.IS_EMPTY_DATA_BAD_RECORD, carbonDataLoadSchema, aggTables, aggTableName, isDirectLoad,
+      loadMetadataDetails, segmentUpdateDetails, segmentUpdateStatusManager)
+
+  }
 
 
 }
---
+++ streaming/src/main/scala/writing/CarbonStreamingTablePath.scala
@@ -0,0 +1,8 @@
+package writing
+
+/**
+  * Created by simar on 17/5/17.
+  */
+class CarbonStreamingTablePath {
+
+}

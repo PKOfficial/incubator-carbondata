bff24e9d1aea7f70febfd942473884ccce31394b
Geetika gupta <geetika.gupta@knoldus.in>
2017-05-19 18:20:07 +0530

    Added functionality for parsing JSON: WIP

--- streaming/src/main/scala/org/apache/streaming/store/StreamReader.scala
+++ streaming/src/main/scala/org/apache/streaming/store/StreamReader.scala
@@ -1,7 +1,8 @@
 package org.apache.streaming.store
 
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.streaming.session.SessionCreator
 import org.apache.streaming.socket.{JsonParser, SocketRead}
 
 
@@ -13,52 +14,66 @@ object StreamReader {
       Map("name" -> StringType, "age" -> IntegerType),
       "./csv/my.csv")*/
     //val readCsv = new SparkReadingCSV
-    //    val sparkSession: SparkSession = SessionCreator.getSparkSession()
-
-    val spark = SparkSession.builder
-      .appName("StreamLocallyExample")
-      .config("spark.master", "local")
-      .getOrCreate()
-    spark.sparkContext.setLogLevel("ERROR")
+    val sparkSession: SparkSession = SessionCreator.getSparkSession()
+    sparkSession.sparkContext.setLogLevel("ERROR")
     //val df: DataFrame = readCsv.getCsvDataFrame(sparkSession, tableDetailForCsv)
     val tableDetailForSocket = TableDetails("tableName",
       "databaseName",
       "/storepath",
       Map("value" -> StringType))
     val readSocketData = new SocketRead
-    val socketDf: DataFrame = readSocketData.getSocketDataFrame(spark)
+    val socketDf: DataFrame = readSocketData.getSocketDataFrame(sparkSession)
     println("here------------I am")
+
+    import sparkSession.implicits._
+    /* val dataset = socketDf.map((x: Row) => x.getAs[String]("value"))
+     val staticDf = sparkSession.sqlContext.read.json(dataset.rdd)
+     staticDf.printSchema()
+    socketDf.map { (x: Row) =>
+      val df = sparkSession.sqlContext.read.json(x)
+      df.show()
+    }*/
     socketDf.as[String].map { (data: String) =>
-      val x: Option[Any] = JsonParser.parseJson(data)
-      if (x.isDefined) {
-        x.fold() { (data: Any) =>
-          data match {
-            case list: List[_] => list
-              .map((rowMap: Any) => getDataFromMap(rowMap, tableDetailForSocket.schema)) //spark.sparkContext.parallelize(list)
-            case _ => ""
+      val parsedJsonString: Option[Any] = JsonParser.parseJson(data)
+      if (parsedJsonString.isDefined) {
+        val x: Unit = parsedJsonString.fold() { (data: Any) =>
+          val y = data match {
+            case list: List[Map[_,_]] => list
+              .map((rowMap: Map[_, _]) => getDataFromMap(rowMap,
+                tableDetailForSocket.schema)) //spark.sparkContext.parallelize(list)
+            case colMap: Map[_,_] => getDataFromMap(colMap, tableDetailForSocket.schema)
           }
         }
       }
     }
 
-
     //    socketDf.as[String].map((x: String) => println("!!!" +x))
     //    sparkSession.streams.awaitAnyTermination()
+    val query = socketDf.select("value").writeStream.outputMode("append").format("console").start()
+    println("!!QUERY ID!!" + query.id)
+    println("!!QUERY RUN ID!!" + query.runId)
+    println("!!QUERY EXPLAIN!!" + query.explain)
 
-    val query = socketDf.writeStream.outputMode("append").format("console").start()
-      .awaitTermination()
+
+    query.awaitTermination()
   }
 
-  def getDataFromMap(rowMap: Any, tableSchema : Map[String, DataType]): Unit = {
-    rowMap match {
-      case rowData: Map[String,_] => rowData map { case (colName, colValue) => parseValue(tableSchema.get(colName))
+  def getDataFromMap(rowMap: Any, tableSchema: Map[String, DataType]): List[(String, DataType)] = {
+    val x: List[(String, DataType)] =(rowMap match {
+      case rowData: Map[String, _] => rowData map { case (colName, colValue) =>
+        val dataType: DataType = tableSchema.get(colName).get
+        val y: Class[_ <: DataType] = dataType.getClass
+        val value = colValue.asInstanceOf[DataType] /*parseValue(tableSchema.get(colName))*/
+        (colName, value)
       }
-    }
+    }).toList
+    println("LIst for data>>>>>>>>>>>>>" + x)
+    x
   }
 
   import org.apache.carbondata.core.metadata.datatype.{DataType => CarbonDataType}
 
-  def parseValue(datatype : DataType): CarbonDataType = {
+  def parseValue(datatype: DataType): CarbonDataType = {
     datatype match {
       case IntegerType => CarbonDataType.INT
       case StringType => CarbonDataType.STRING
